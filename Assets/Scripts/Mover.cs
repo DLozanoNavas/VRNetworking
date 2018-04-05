0ad2a5b2bfdd9172d4d75d429bfa6fbd72ca927e
Daniel Fernando Lozano <cto@fenix-alliance.com>
2018-04-05 03:57:11 -0500

    Updates Rails, terrain and adds packet and packet rails

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,37 +2,89 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Mover : MonoBehaviour {
+public class Mover : MonoBehaviour
+{
 
     public Rail rail;
+    public PlayMode mode;
+    public float speed = 2.5f;
     private int currentSeg;
     private float transition;
     private bool isCompleted;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    public bool isReversed;
+    public bool isLooping;
+    public bool pingPong;
+
+
+    // Update is called once per frame
+    void Update()
+    {
         if (!rail)
             return;
         if (!isCompleted)
-            Play();
-	}
-    private void Play()
+            Play(!isReversed);
+    }
+    private void Play(bool forward = true)
     {
-        transition += Time.deltaTime * 1 / 2.5f;
+        float m = (rail.nodes[currentSeg + 1].position - rail.nodes[currentSeg].position).magnitude;
+        float s = (Time.deltaTime * 1 / m) * speed;
+        transition += (forward) ? s : -s;
         if (transition > 1)
         {
             transition = 0;
             currentSeg++;
-        }else if(transition < 0)
+            if (currentSeg == rail.nodes.Length - 1)
+            {
+                if (isLooping)
+                {
+                    if (pingPong)
+                    {
+                        transition = 1;
+                        currentSeg = rail.nodes.Length - 2;
+                        isReversed = !isReversed;
+                    }
+                    else
+                    {
+                        currentSeg = 0;
+                    }
+
+                }
+                else
+                {
+                    isCompleted = true;
+                    return;
+                }
+            }
+        }
+        else if (transition < 0)
         {
             transition = 1;
             currentSeg--;
+            if (currentSeg == - 1)
+            {
+                if (isLooping)
+                {
+                    if (pingPong)
+                    {
+                        transition = 0;
+                        currentSeg = 0;
+                        isReversed = !isReversed;
+                    }
+                    else
+                    {
+                        currentSeg = rail.nodes.Length -2;
+                    }
+
+                }
+                else
+                {
+                    isCompleted = true;
+                    return;
+                }
+            }
         }
-        transform.position = rail.LinearPosition(currentSeg, transition);
-        transform.rotation = rail.Orientation(currentSeg, transform);
+        //transform.position = rail.LinearPosition(currentSeg, transition);
+        transform.position = rail.PositionOnRail(currentSeg, transition, mode);
+        transform.rotation = rail.Orientation(currentSeg, transition);
     }
 }
