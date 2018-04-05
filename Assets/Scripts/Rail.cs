a6080e7b0f4d7b2e41994ba68bcb6b4a173a7fe8
Daniel Fernando Lozano <cto@fenix-alliance.com>
2018-04-04 20:12:27 -0500

    Updates Rail.cs

--- Assets/Scripts/Rail.cs
+++ Assets/Scripts/Rail.cs
@@ -1,16 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEditor;
+[ExecuteInEditMode]
 public class Rail : MonoBehaviour {
+    private Transform[] nodes;
+    private void Start()
+    {
+        nodes = GetComponentsInChildren<Transform>();
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Vector3 LinearPosition(int seg, float ratio)
+    {
+        Vector3 p1 = nodes[seg].position;
+        Vector3 p2 = nodes[seg + 1].position;
+        return Vector3.Lerp(p1, p2, ratio);
+    }
+    public Quaternion Orientation(int seg,float ratio)
+    {
+        Quaternion q1 = nodes[seg].rotation;
+        Quaternion q2 = nodes[seg + 1].rotation;
+        return Quaternion.Lerp(q1, q2, ratio);
+    }
+    private void OnDrawGizmos()
+    {
+        for (int i = 0; i < nodes.Length-1; i++)
+        {
+            Handles.DrawDottedLine(nodes[i].position, nodes[i + 1].position, 3.0f);
+        }
+    }
 }
