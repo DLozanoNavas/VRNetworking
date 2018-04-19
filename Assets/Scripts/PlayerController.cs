b86edfdc30d029b27465644af0e5bc8d92a65bda
Daniel Fernando Lozano <cto@fenix-alliance.com>
2018-04-19 18:13:16 -0500

    Updates Sound triggers

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,25 +4,70 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-    void OnTriggerEnter(Collider otherObj)
-    {
-
-            if (otherObj.name == "OSILayer2")
-            {
-                Destroy(otherObj.gameObject);
-                FindObjectOfType<AudioManager>().Play("2-Stage");
-                Debug.Log("Packet eliminated");
-            }
-
+    // Use this for initialization
+    void Start() {
 
     }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+    void OnTriggerEnter(Collider otherObj)
+    {
+        //switch (otherObj.name)
+        //{
+        //    case "OSILayer2":
+        //        FindObjectOfType<AudioManager>().Play("2-Stage");
+        //        break;
+        //    case "OSILayer3":
+        //        FindObjectOfType<AudioManager>().Play("3-Stage");
+        //        break;
+        //    case "OSILayer4":
+        //        FindObjectOfType<AudioManager>().Play("4-Stage");
+        //        break;
+        //    case "OSILayer5":
+        //        FindObjectOfType<AudioManager>().Play("5-Stage");
+        //        break;
+        //    case "OSILayer6":
+        //        FindObjectOfType<AudioManager>().Play("6-Stage");
+        //        break;
+        //    case "OSILayer7":
+        //        FindObjectOfType<AudioManager>().Play("7-Stage");
+        //        break;
+        //    case "OSILayer8":
+        //        FindObjectOfType<AudioManager>().Play("8-Stage");
+        //        break;
+        //}
+
+        if (otherObj.name == "OSILayer2")
+        {
+            FindObjectOfType<AudioManager>().Play("2-Stage");
+        }
+        else if (otherObj.name == "OSILayer3")
+        {
+            FindObjectOfType<AudioManager>().Play("3-Stage");
+        }
+        else if(otherObj.name == "OSILayer4")
+        {
+            FindObjectOfType<AudioManager>().Play("4-Stage");
+        }
+        else if(otherObj.name == "OSILayer5")
+        {
+            FindObjectOfType<AudioManager>().Play("5-Stage");
+        }
+        else if(otherObj.name == "OSILayer6")
+        {
+            FindObjectOfType<AudioManager>().Play("6-Stage");
+        }
+        else if(otherObj.name == "OSILayer7")
+        {
+            FindObjectOfType<AudioManager>().Play("7-Stage");
+        }
+        else if(otherObj.name == "OSILayer8")
+        {
+            FindObjectOfType<AudioManager>().Play("8-Stage");
+        }
+
+    } 
 }
