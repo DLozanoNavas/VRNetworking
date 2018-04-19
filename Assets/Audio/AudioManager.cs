15507453b4f717164e2b6f60d71b4e67361c1e34
Daniel Fernando Lozano <cto@fenix-alliance.com>
2018-04-19 11:33:10 -0500

    Modifies Audio Manager and adds AudioSource to Sounds class.

--- Assets/Audio/AudioManager.cs
+++ Assets/Audio/AudioManager.cs
@@ -7,7 +7,14 @@ public class AudioManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Awake () {
-		
+		foreach(Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+        }
 	}
 	
 	// Update is called once per frame
