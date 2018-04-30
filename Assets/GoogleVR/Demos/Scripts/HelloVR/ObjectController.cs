d21ac5b6caf19bebf22764682e67d23ddcb05028
Daniel Fernando Lozano <cto@fenix-alliance.com>
2018-04-30 12:11:05 -0500

    Uplevels Router anim controller and display's messages when packet gazed At returns true

--- Assets/GoogleVR/Demos/Scripts/HelloVR/ObjectController.cs
+++ Assets/GoogleVR/Demos/Scripts/HelloVR/ObjectController.cs
@@ -12,73 +12,102 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-namespace GoogleVR.HelloVR {
-  using UnityEngine;
+namespace GoogleVR.HelloVR
+{
+    using UnityEngine;
+    using UnityEngine.UI;
 
-  [RequireComponent(typeof(Collider))]
-  public class ObjectController : MonoBehaviour {
-    private Vector3 startingPosition;
-    private Renderer renderer;
+    [RequireComponent(typeof(Collider))]
+    public class ObjectController : MonoBehaviour
+    {
+        private Vector3 startingPosition;
+        private Renderer renderer;
+        
+        public Material inactiveMaterial;
+        public GameObject infoMessageCanvas;
+        public Text infoMessageText;
+        public Material gazedAtMaterial;
+        private const string INFO_MESSAGE_CANVAS_NAME = "InfoMessageCanvas";
+        private const string INFO_MESSAGE_TEXT_NAME = "InfoMessageText";
+        private const string INFO_MESSAGE_FACEBOOK_PACKET = "facebook.com";
+        void Start()
+        {
+            startingPosition = transform.localPosition;
+            renderer = GetComponent<Renderer>();
+            SetGazedAt(false);
+            if (infoMessageCanvas == null)
+            {
+                infoMessageCanvas = transform.Find(INFO_MESSAGE_CANVAS_NAME).gameObject;
+                if (infoMessageCanvas != null)
+                {
+                    infoMessageText = infoMessageCanvas.transform.Find(INFO_MESSAGE_TEXT_NAME).GetComponent<Text>();
+                }
+            }
+            // Message canvas will be enabled later when there's a message to display.
+            infoMessageCanvas.SetActive(false);
+        }
 
-    public Material inactiveMaterial;
-    public Material gazedAtMaterial;
+        public void SetGazedAt(bool gazedAt)
+        {
+            if (inactiveMaterial != null && gazedAtMaterial != null)
+            {
+                // TODO: Add random info to packets
+                infoMessageText.text = INFO_MESSAGE_FACEBOOK_PACKET;
+                infoMessageCanvas.SetActive(true);
+                renderer.material = gazedAt ? gazedAtMaterial : inactiveMaterial;
+                return;
+            }
 
-    void Start() {
-      startingPosition = transform.localPosition;
-      renderer = GetComponent<Renderer>();
-      SetGazedAt(false);
-    }
+        }
 
-    public void SetGazedAt(bool gazedAt) {
-      if (inactiveMaterial != null && gazedAtMaterial != null) {
-        renderer.material = gazedAt ? gazedAtMaterial : inactiveMaterial;
-        return;
-      }
-    }
+        public void Reset()
+        {
+            int sibIdx = transform.GetSiblingIndex();
+            int numSibs = transform.parent.childCount;
+            for (int i = 0; i < numSibs; i++)
+            {
+                GameObject sib = transform.parent.GetChild(i).gameObject;
+                sib.transform.localPosition = startingPosition;
+                sib.SetActive(i == sibIdx);
+            }
+        }
 
-    public void Reset() {
-      int sibIdx = transform.GetSiblingIndex();
-      int numSibs = transform.parent.childCount;
-      for (int i=0; i<numSibs; i++) {
-        GameObject sib = transform.parent.GetChild(i).gameObject;
-        sib.transform.localPosition = startingPosition;
-        sib.SetActive(i == sibIdx);
-      }
-    }
-
-    public void Recenter() {
+        public void Recenter()
+        {
 #if !UNITY_EDITOR
       GvrCardboardHelpers.Recenter();
 #else
-      if (GvrEditorEmulator.Instance != null) {
-        GvrEditorEmulator.Instance.Recenter();
-      }
+            if (GvrEditorEmulator.Instance != null)
+            {
+                GvrEditorEmulator.Instance.Recenter();
+            }
 #endif  // !UNITY_EDITOR
+        }
+
+        public void TeleportRandomly()
+        {
+            // Pick a random sibling, move them somewhere random, activate them,
+            // deactivate ourself.
+            int sibIdx = transform.GetSiblingIndex();
+            int numSibs = transform.parent.childCount;
+            sibIdx = (sibIdx + Random.Range(1, numSibs)) % numSibs;
+            GameObject randomSib = transform.parent.GetChild(sibIdx).gameObject;
+
+            // Move to random new location ±100º horzontal.
+            Vector3 direction = Quaternion.Euler(
+                0,
+                Random.Range(-90, 90),
+                0) * Vector3.forward;
+            // New location between 1.5m and 3.5m.
+            float distance = 2 * Random.value + 1.5f;
+            Vector3 newPos = direction * distance;
+            // Limit vertical position to be fully in the room.
+            newPos.y = Mathf.Clamp(newPos.y, -1.2f, 4f);
+            randomSib.transform.localPosition = newPos;
+
+            randomSib.SetActive(true);
+            gameObject.SetActive(false);
+            SetGazedAt(false);
+        }
     }
-
-    public void TeleportRandomly() {
-      // Pick a random sibling, move them somewhere random, activate them,
-      // deactivate ourself.
-      int sibIdx = transform.GetSiblingIndex();
-      int numSibs = transform.parent.childCount;
-      sibIdx = (sibIdx + Random.Range(1, numSibs)) % numSibs;
-      GameObject randomSib = transform.parent.GetChild(sibIdx).gameObject;
-
-      // Move to random new location ±100º horzontal.
-      Vector3 direction = Quaternion.Euler(
-          0,
-          Random.Range(-90, 90),
-          0) * Vector3.forward;
-      // New location between 1.5m and 3.5m.
-      float distance = 2 * Random.value + 1.5f;
-      Vector3 newPos = direction * distance;
-      // Limit vertical position to be fully in the room.
-      newPos.y = Mathf.Clamp(newPos.y, -1.2f, 4f);
-      randomSib.transform.localPosition = newPos;
-
-      randomSib.SetActive(true);
-      gameObject.SetActive(false);
-      SetGazedAt(false);
-    }
-  }
 }
